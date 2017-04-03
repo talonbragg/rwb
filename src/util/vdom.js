8296bff223d977c2652e0395545f00e22a4acf6a
talonbragg <pvpgamecrash@gmail.com>
2017-04-02 19:48:50 -0700

    Start virtual dom

---
+++ src/util/vdom.js
@@ -0,0 +1,14 @@
+function h(type, props, ...children) {
+  return { type, props, children };
+}
+
+function createElement(node) {
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  }
+  const $el = document.createElement(node.type);
+  node.children
+    .map(createElement)
+    .forEach($el.appendChild.bind($el));
+  return $el;
+}
